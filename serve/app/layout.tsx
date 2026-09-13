6c37be0c08167781f3965bd734b2669a35884522
Joswin D'Souza <joswinlynel@gmail.com>
2026-09-13 13:30:36 +0100

    Suppress hydration warnings to fix bis_skin_checked errors from browser extensions

--- serve/app/layout.tsx
+++ serve/app/layout.tsx
@@ -26,8 +26,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${inter.variable} ${playfair.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col font-sans bg-background text-foreground">
+      <body className="min-h-full flex flex-col font-sans bg-background text-foreground" suppressHydrationWarning>
         {children}
       </body>
     </html>
